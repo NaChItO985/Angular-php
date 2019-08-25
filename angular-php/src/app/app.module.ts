0dd6d42e892dd08fb3c02b01350672576f153526
Nahum <nachito_985@hotmail.com>
2019-08-25 12:45:50 -0500

    Basic structure of page

--- angular-php/src/app/app.module.ts
+++ angular-php/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminpageComponent } from './components/adminpage/adminpage.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent,
+    AdminpageComponent,
+    NotFoundComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
