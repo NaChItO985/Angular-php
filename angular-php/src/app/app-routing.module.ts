0b85d8e268079f5ab8ee7bfc75e7c3aa08e6a625
Nahum <nachito_985@hotmail.com>
2019-08-25 15:24:11 -0500

    Update navbar and add routing

--- angular-php/src/app/app-routing.module.ts
+++ angular-php/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/Signup/signup.component';
+import { AdminpageComponent } from './components/adminpage/adminpage.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
